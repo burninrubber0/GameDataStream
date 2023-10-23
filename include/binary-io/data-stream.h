acb7a574a0b44c5d96ba06098fb780b87ab6bb06
burninrubber0 <burninrubber0@gmail.com>
2023-10-23 09:38:45 -0400

    Initial commit

---
+++ include/binary-io/data-stream.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <QDataStream>
+#include <QFile> // For seeks and device flags
+
+class DataStream : public QDataStream
+{
+public:
+	enum class Platform
+	{
+		PS3,
+		X360,
+		PC,
+		PS4,
+		NX
+	};
+
+	DataStream(Platform platform = Platform::PC);
+
+	// Get the stream's current platform
+	Platform getPlatform();
+
+	// Set the stream's currrent platform and update details
+	void setPlatform(Platform platform);
+
+	// Get whether the stream reads and writes pointers as 32 or 64 bit
+	bool getIs64Bit();
+
+	// Set whether the stream reads and writes pointers as 32 or 64 bit
+	void setIs64Bit(bool setting);
+
+	// Reads a pointer from this stream into an integer variable
+	void readPtr(quint64& ptr);
+
+	// Writes an integer value to a pointer variable
+	void writePtr(quint64 ptr);
+
+	// Reads a size_t field from this stream into an integer variable
+	void readSize(size_t& size);
+
+	// Writes an integer value to a size_t variable
+	void writeSize(size_t size);
+
+	// Reads an unspecified number of bytes from this stream into a string
+	// Ends when a null terminator is reached
+	void readString(QString& string);
+
+	// Reads a specified number of bytes from this stream into a string
+	void readString(QString& string, quint32 length);
+
+	// Writes a string to the stream, optionally limited to a maximum length and/or null terminated
+	void writeString(QString string, bool nullTerminated = false, qint64 length = -1);
+
+	// Reads a specified number of bytes from this stream to another stream, writing to a specified
+	// offset in the other stream. By default, this offset is the current offset of this stream.
+	// This is most useful for storing unknown data and padding to prevent data loss
+	void readRawData(DataStream& stream, quint32 length, qint64 position = -1);
+
+	// Get the current offset the device is reading/writing from
+	qint64 pos();
+
+	// Opens the device
+	void open(QIODeviceBase::OpenMode mode);
+
+	// Seeks to a specified offset in the device
+	void seek(qint64 offset);
+
+	QByteArray peek(qint64 maxlen);
+
+	// Seeks to a position which is a specified length beyond the current offset
+	void skip(qint64 length);
+
+	// Closes the device
+	void close();
+
+private:
+	Platform platform;
+	bool is64Bit = false;
+
+	// Set endianness based on platform (default is big endian)
+	void setDefaultEndianness();
+
+	// Set whether to read as 64 bit based on platform (default is false)
+	void setDefaultIs64Bit();
+};
---
+++ src/binary-io/data-stream.cpp
@@ -0,0 +1,170 @@
+#include <binary-io/data-stream.h>
+
+DataStream::DataStream(Platform platform)
+	: platform(platform)
+{
+	// Set platform-specific details
+	setPlatform(platform);
+
+	// Set single precision FP (default is double)
+	setFloatingPointPrecision(QDataStream::SinglePrecision);
+}
+
+// Get the stream's current platform
+DataStream::Platform DataStream::getPlatform()
+{
+	return platform;
+}
+
+// Set the stream's current platform and update details
+void DataStream::setPlatform(Platform platform)
+{
+	this->platform = platform;
+	setDefaultEndianness();
+	setDefaultIs64Bit();
+}
+
+// Get whether the stream reads and writes pointers as 32 or 64 bit
+bool DataStream::getIs64Bit()
+{
+	return is64Bit;
+}
+
+// Set whether the stream reads and writes pointers as 32 or 64 bit
+void DataStream::setIs64Bit(bool setting)
+{
+	is64Bit = setting;
+}
+
+// Reads a pointer from this stream into an integer variable
+void DataStream::readPtr(quint64& ptr)
+{
+	if (!is64Bit)
+		*this >> (quint32&)ptr;
+	else
+		*this >> ptr;
+}
+
+// Writes an integer value to a pointer variable
+void DataStream::writePtr(quint64 ptr)
+{
+	if (!is64Bit)
+		*this << (quint32)ptr;
+	else
+		*this << ptr;
+}
+
+// Reads a size_t field from this stream into an integer variable
+void DataStream::readSize(size_t& size)
+{
+	readPtr(size);
+}
+
+// Writes an integer value to a size_t variable
+void DataStream::writeSize(size_t size)
+{
+	writePtr(size);
+}
+
+// Reads an unspecified number of bytes from this stream into a string
+// Ends when a null terminator is reached
+void DataStream::readString(QString& string)
+{
+	string.clear();
+	char c;
+	while (!device()->atEnd())
+	{
+		*this >> c;
+		if (c == '\0')
+			break;
+		string.append(c);
+	}
+}
+
+// Reads a specified number of bytes from this stream into a string
+void DataStream::readString(QString& string, quint32 length)
+{
+	string = QString::fromLatin1((device()->read(length)));
+}
+
+// Writes a string to the stream, optionally limited to a maximum length and/or null terminated
+void DataStream::writeString(QString string, bool nullTerminated, qint64 length)
+{
+	device()->write(string.toLatin1(), length == -1 ? string.size() : length);
+	if (nullTerminated)
+		*this << '\0';
+}
+
+// Reads a specified number of bytes from this stream to another stream, writing to a specified
+// offset in the other stream. By default, this offset is the current offset of this stream.
+// This is most useful for storing unknown data and padding to prevent data loss
+void DataStream::readRawData(DataStream& stream, quint32 length, qint64 position)
+{
+	position != -1 ? stream.seek(position) : stream.seek(pos());
+	stream.device()->write(device()->read(length));
+}
+
+// Get the current offset the device is reading/writing from
+qint64 DataStream::pos()
+{
+	return device()->pos();
+}
+
+// Opens the device
+void DataStream::open(QIODeviceBase::OpenMode mode)
+{
+	device()->open(mode);
+}
+
+// Seeks to a specified offset in the device
+void DataStream::seek(qint64 offset)
+{
+	device()->seek(offset);
+}
+
+QByteArray DataStream::peek(qint64 maxlen)
+{
+	return device()->peek(maxlen);
+}
+
+// Seeks to a position which is a specified length beyond the current offset
+void DataStream::skip(qint64 length)
+{
+	device()->seek(pos() + length);
+}
+
+// Closes the device
+void DataStream::close()
+{
+	device()->close();
+}
+
+// Set endianness based on platform (default is big endian)
+void DataStream::setDefaultEndianness()
+{
+	switch (platform)
+	{
+	case Platform::PS3:
+	case Platform::X360:
+		setByteOrder(QDataStream::BigEndian);
+		break;
+	default:
+		setByteOrder(QDataStream::LittleEndian);
+		break;
+	}
+}
+
+// Set whether to read as 64 bit based on platform (default is false)
+void DataStream::setDefaultIs64Bit()
+{
+	switch (platform)
+	{
+	case Platform::PS4:
+	case Platform::NX:
+		setIs64Bit(true);
+		break;
+	default:
+		setIs64Bit(false);
+		break;
+	}
+}
